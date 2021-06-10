eecc1908368aa95a656406153354649c427ef051
LoZack19 <giozaccaria02@gmail.com>
2021-06-10 20:59:15 +0200

    Merge branch 'main' of https://github.com/LoZack19/butos

--- programs/main.c
+++ programs/main.c
@@ -7,7 +7,7 @@ char* carriage_return(char volatile* cursor);
 
 void _start()
 {
-    
+    disable_cursor();
     for (int i = 0; i < 25; i++)
         print_pm(0b00000111, "Lanfredi gay, ma a 32 bit\n\r");
     
