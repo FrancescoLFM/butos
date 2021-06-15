b7af5485eb748906470f16fcbb6691bab3b3c424
LoZack19 <giozaccaria02@gmail.com>
2021-06-15 20:09:50 +0200

    test a20 line

--- clibs/def.h
+++ clibs/def.h
@@ -1,9 +1,15 @@
 #ifndef DEFINE_H
 #define DEFINE_H
 
+#include <stdint.h>
+
 #define NULL    ((void*)(0))
 
 typedef unsigned long int size_t;
-typedef unsigned char uint8_t;
+
+typedef unsigned long int   u32;
+typedef unsigned short int  u16;
+typedef unsigned char       u8;
+
 
 #endif
--- programs/prova.c
+++ programs/prova.c
@@ -6,24 +6,34 @@ void puts(char* string);
 
 char* htos(char* buffer, uint32_t hex, uint8_t size);
 
-void main();
+uint8_t test_a20();
 
 void _start()
 {
-    main();
-    stop();
-}
-
-void main()
-{
     char numero[9];
-    uint8_t len = strlen("Francesco");
-    puts(htos(numero, len, sizeof(len)));
+    uint8_t test = test_a20();
+    puts((test) ? "a20 line enabled" : "a20 line disabled");
+    stop();
 }
 
 void puts(char* string)
 {
     print_pm(WHITE_ON_BLACK, string);
+}
+
+uint8_t test_a20()
+{
+    uint8_t temp, ret;
+    uint8_t *mem1 = (uint8_t*)(0x112345);
+    uint8_t *mem2 = (uint8_t*)(0x012345);
+
+    temp = *mem1;
+
+    *mem1 = ~(*mem2);
+    ret = (*mem1 != *mem2);
+
+    *mem1 = temp;
+    return ret;
 }
 
 char* htos(char* buffer, uint32_t hex, uint8_t size)
