b5f1dbeacc5bd9c1ed200e0953a69370e9217b64
LoZack19 <giozaccaria02@gmail.com>
2021-06-16 14:26:11 +0200

    automatic sector detection

--- programs/configure.sh
+++ programs/configure.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-len=$(bc <<< "($(stat --printf %s $1) / 512 + 1)")
-echo "SECTORS: $len"
-len=$(($len * 512))
-echo "SIZE: $len"
-dd if=/dev/zero of=$1 bs=1 count=1 seek=$((len - 1))
+size=$(stat --printf %s $1)
+
+len=$(bc <<< "($size / 512 + 1)")
+
+if [ $(expr $size % 512) != 0 ]
+then
+    tmp=$(expr $len \* 512)
+    dd if=/dev/zero of=$1 bs=1 count=1 seek=$((tmp - 1)) 2>/dev/null 1>/dev/null
+else
+    len=$(expr $len - 1)
+fi
+
+printf "0x%x" $len
 exit
